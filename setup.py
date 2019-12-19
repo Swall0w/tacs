23f28a947391762fd086a43750b93546ed2a33c7
Swall0w <technext.jpn@gmail.com>
2019-12-19 14:24:34 +0900

    [update]: setup.py for PyPI.

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@
 
 from setuptools import setup
 
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name="tacs",
     version="0.0.1",
     author="Swall0w",
     author_email="",
     description="TOML yet Another Configuration System",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/Swall0w/tacs",
     packages=["tacs"],
-    long_description="A simple experiment configuration system for research in TOML",
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
@@ -20,5 +26,5 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    install_requires=["toml"],
+    install_requires=["toml",],
 )
